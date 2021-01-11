5e851c1a5e31d9f345032bc47312c2d4ecaa51ea
AnantaYudica <>
2021-01-11 23:51:57 +0700

    Added implementation member function SetValue

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -37,6 +37,22 @@ void Calculator::UpdateValue(const int& num)
     m_update = true;
 }
 
+void Calculator::SetValue()
+{
+    m_comma = false;
+    m_ans_ndigit = m_ndigit;
+    m_ndigit = 0;
+    m_ans_int_ndigit = m_int_ndigit;
+    m_int_ndigit = 0;
+    m_ans_fraction_ndigit = m_fraction_ndigit; 
+    m_fraction_ndigit = 0;
+    m_ans_int = m_int;
+    m_int = 0;
+    m_ans_fraction = m_fraction; 
+    m_fraction = 0;
+    m_update = true;
+}
+
 void Calculator::Key(char ch)
 {
     std::cout << "ch : " << ch << std::endl;
