f93d8e612496a4371d8790d04ab7b94474eaa590
AnantaYudica <>
2021-01-11 23:49:41 +0700

    Updated constructor

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -3,9 +3,19 @@
 #include <iostream>
 
 Calculator::Calculator() :
-    m_curr(0),
-    m_tmp(0),
-    m_op('\0')
+    m_update(false),
+    m_comma(false),
+    m_op('\0'),
+    m_ndigit(0), 
+    m_ans_ndigit(0),
+    m_int_ndigit(0),
+    m_ans_int_ndigit(0),
+    m_fraction_ndigit(0), 
+    m_ans_fraction_ndigit(0),
+    m_int(0), 
+    m_ans_int(0),
+    m_fraction(0), 
+    m_ans_fraction(0)
 {}
 
 Calculator::~Calculator()
